c5b2c9b134053256a7608c5cf630b07f428c8956
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-06-21 13:22:57 +0600

    added 3 screens

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+import icons from './icons';
+import images from './images';
+/* eslint-disable prettier/prettier */
+import theme, { COLORS, FONTS, SIZES } from './theme';
+
+export { icons, images, theme, COLORS, SIZES, FONTS };
+
